045208a95e21d8a4e4b771e3f0a7256d1d2007d9
jather <31395687+jather@users.noreply.github.com>
2025-08-30 09:35:20 +1000

    calculator

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
+import sys
 from google import genai
+from google.genai import types
 
 
 load_dotenv()
@@ -9,17 +11,37 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
     client = genai.Client(api_key=api_key)
-    content = client.models.generate_content(
+    prompt = ""
+    flag = ""
+    verbose = False
+
+    if len(sys.argv) > 1:
+        prompt = sys.argv[1]
+    else:
+        print("prompt must be provided as first argument")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        flag = sys.argv[2]
+    if flag == "--verbose":
+        verbose = True
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
-    print(api_key)
-    print(content.text)
+    print(response.text)
     promt_tokens, usage_tokens = (
-        content.usage_metadata.prompt_token_count,
-        content.usage_metadata.candidates_token_count,
+        response.usage_metadata.prompt_token_count,
+        response.usage_metadata.candidates_token_count,
     )
-    print(f"Prompt tokens: {promt_tokens}\nResponse tokens: {usage_tokens}")
+    if verbose:
+        print(
+            f"User prompt: {prompt}\nPrompt tokens: {promt_tokens}\nResponse tokens: {usage_tokens}"
+        )
 
 
 if __name__ == "__main__":
