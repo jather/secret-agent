611dad1d80841691eac27f8e2a355965500da818
jather <31395687+jather@users.noreply.github.com>
2026-03-17 13:54:25 +1100

    argparse

--- main.py
+++ main.py
@@ -3,45 +3,38 @@ import os
 import sys
 from google import genai
 from google.genai import types
-
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None:
+    raise RuntimeError("no api key found")
 
 
 def main():
+    parser = argparse.ArgumentParser(description="secret agent for coding")
+    parser.add_argument("user_prompt", type=str, help="Prompt given by user")
+    args = parser.parse_args()
     client = genai.Client(api_key=api_key)
-    prompt = ""
-    flag = ""
-    verbose = False
 
-    if len(sys.argv) > 1:
-        prompt = sys.argv[1]
-    else:
-        print("prompt must be provided as first argument")
-        sys.exit(1)
-    if len(sys.argv) > 2:
-        flag = sys.argv[2]
-    if flag == "--verbose":
-        verbose = True
-
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=prompt)]),
-    ]
-
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
+        model="gemini-2.5-flash",
         contents=messages,
     )
+    # messages = [
+    #     types.Content(role="user", parts=[types.Part(text=prompt)]),
+    # ]
+
     print(response.text)
     promt_tokens, usage_tokens = (
         response.usage_metadata.prompt_token_count,
         response.usage_metadata.candidates_token_count,
     )
-    if verbose:
-        print(
-            f"User prompt: {prompt}\nPrompt tokens: {promt_tokens}\nResponse tokens: {usage_tokens}"
-        )
+    # if verbose:
+    print(
+        f"User prompt: {args.user_prompt}\nPrompt tokens: {promt_tokens}\nResponse tokens: {usage_tokens}"
+    )
 
 
 if __name__ == "__main__":
