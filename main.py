e0cdc6de8bd51014eaa98402ef112bc80d514959
jather <31395687+jather@users.noreply.github.com>
2026-03-27 22:40:54 +1100

    let agent call functions

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google import genai
 from google.genai import types
 import argparse
 from prompts import system_prompt
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -31,9 +31,21 @@ def main():
     )
 
     print(response.text)
+    function_results = []
     if response.function_calls:
         for function in response.function_calls:
-            print(f"Calling function: {function.name}({function.args})")
+            function_call_result = call_function(function, args.verbose)
+            if not function_call_result.parts:
+                raise Exception(
+                    "function call result should have non-empty .parts list"
+                )
+            if not function_call_result.parts[0].function_response:
+                raise Exception(
+                    "function call result function_response should not be None"
+                )
+            function_results.append(function_call_result.parts[0])
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
 
     promt_tokens, usage_tokens = (
         response.usage_metadata.prompt_token_count,
